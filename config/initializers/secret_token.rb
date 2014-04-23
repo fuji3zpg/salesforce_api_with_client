dc0a73b2ce10ba27c0e30d739b6317c6cbc228f2
Kazunori Fujimoto <kazu-f@sd5.so-net.ne.jp>
2014-04-23 19:57:15 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SalesforceApi::Application.config.secret_key_base = '151d7f4d53e97350914a233ca8d1ae2bd3b0e6282d64e4385a77d8966d173ce1b8c94f46b07b08ea76aece6b3b0d868e7de4dbf03afe4edc58807fb8eac6c5c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SalesforceApi::Application.config.session_store :cookie_store, key: '_salesforce_api_session'
