746bf2d9e1884fcf953638f176922a17dd8f9fe5
Kazunori Fujimoto <kazu-f@sd5.so-net.ne.jp>
2014-04-24 21:44:07 +0900

    enable to show users list and each user

---
+++ app/controllers/salesforce_users_controller.rb
@@ -0,0 +1,21 @@
+# class SalesforceUsersController < ApplicationController
+class SalesforceUsersController < ApplicationController
+  def index
+    salesforce_client
+    user_class = @client.materialize("User")
+    @users = User.all
+  end
+
+  def show
+    salesforce_client
+    user_class = @client.materialize("User")
+    @user = User.find_by_Id(params[:id])
+  end
+
+  private
+
+  def salesforce_client
+    @client = Databasedotcom::Client.new client_id: ENV['DATABASEDOTCOM_CLIENT_ID'], client_secret: ENV['DATABASEDOTCOM_CLIENT_SECRET']
+    @client.authenticate :username => ENV['DATABASEDOTCOM_CLIENT_USERNAME'], :password => ENV['DATABASEDOTCOM_CLIENT_AUTHENTICATE_PASSWORD']
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SalesforceApi::Application.routes.draw do
+  get 'salesforce_users/index' => 'salesforce_users#index'
+  get 'salesforce_users/:id' => 'salesforce_users#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
